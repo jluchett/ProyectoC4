f4d5c842170f105904488ad5441c9e1298a4536c
jluchett <jluchett@hotmail.com>
2022-10-20 22:02:36 -0500

    CRUD completo exitoso de novio

--- proyecto_back/controllers/novios.controller.js
+++ proyecto_back/controllers/novios.controller.js
@@ -28,52 +28,52 @@ exports.createnv = function(req,res){
     })
 }
 
-// exports.findnv = function(req,res){
-//     Novio.find(function(err, novios){
-//         res.json(novios)
-//     })
-// }
+exports.findnv = function(req,res){
+    Novio.find(function(err, novios){
+        res.json(novios)
+    })
+}
 
-// exports.findOnenv = function(req,res){
-//     Novio.findOne({_id: req.params.id},function(err, novio){
-//         res.json(novio)
-//     })
-// }
+exports.findOnenv = function(req,res){
+    Novio.findOne({_id: req.params.id},function(err, novio){
+        res.json(novio)
+    })
+}
 
-// exports.updatenv = function(req,res){
-//     let novio = ({
-//         nombre_novio: req.body.nombre_novio,
-//         edad: req.body.edad,
-//         estatura: req.body.estatura,
-//         color_ojos: req.body.color_ojos,
-//         nacionalidad: req.body.nacionalidad,
-//         etnia: req.body.etnia
-//     })
-//     Novio.findByIdAndUpdate(req.params.id,{$set: novio},function(err){
-//         if(err){
-//             console.error(err),
-//             response.exito = false,
-//             response.msg = "Error al modificar el novio"
-//             res.json(response)
-//             return;
-//         }
-//         response.exito = true,
-//         response.msg = "El novio se actualizo correctamente"
-//         res.json(response)
-//     })
-// }
+exports.updatenv = function(req,res){
+    let novio = ({
+        nombre_novio: req.body.nombre_novio,
+        edad: req.body.edad,
+        estatura: req.body.estatura,
+        color_ojos: req.body.color_ojos,
+        nacionalidad: req.body.nacionalidad,
+        etnia: req.body.etnia
+    })
+    Novio.findByIdAndUpdate(req.params.id,{$set: novio},function(err){
+        if(err){
+            console.error(err),
+            response.exito = false,
+            response.msg = "Error al modificar el novio"
+            res.json(response)
+            return;
+        }
+        response.exito = true,
+        response.msg = "El novio se actualizo correctamente"
+        res.json(response)
+    })
+}
 
-// exports.removenv = function(req,res){
-//     Novio.findByIdAndRemove({_id: req.params.id},function(err){
-//         if(err){
-//             console.error(err)
-//             response.exito = false,
-//             response.msg = "Error al eliminar el novio"
-//             res.json(response)
-//             return;
-//         }
-//         response.exito = true,
-//         response.msg ="El novio se elimino correctamente"
-//         res.json(response)
-//     })
-// }
+exports.removenv = function(req,res){
+    Novio.findByIdAndRemove({_id: req.params.id},function(err){
+        if(err){
+            console.error(err)
+            response.exito = false,
+            response.msg = "Error al eliminar el novio"
+            res.json(response)
+            return;
+        }
+        response.exito = true,
+        response.msg ="El novio se elimino correctamente"
+        res.json(response)
+    })
+}
--- proyecto_back/routes/novios.router.js
+++ proyecto_back/routes/novios.router.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const noviosController = require('../controllers/novios.controller');
 
 router.post("/", noviosController.createnv);
+router.get("/", noviosController.findnv);
+router.get("/:id", noviosController.findOnenv);
+router.put("/:id", noviosController.updatenv);
+router.delete("/:id", noviosController.removenv);
 
 module.exports = router;
